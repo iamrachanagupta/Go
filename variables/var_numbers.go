7234ca6cc0fc2fb1d6e0b0ff4820685e4c064c43
imrachanagupta <rachanagupta04@gmail.com>
2021-10-22 17:45:20 +0530

    usuage of variables

--- variables/var_numbers.go
+++ variables/var_numbers.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var i int = 10
-	fmt.Println(i)
+	var i int // declaration , automatically initialised to 0 by compiler
+	j := 20   /* declaration with initialization without specifying type
+	But it should be followed by its usuage like a print statement otherwise will throw error*/
+	fmt.Println(i) //outputs 0
+	fmt.Println(j) //outputs 20
+	i = 15         //assignment
+	fmt.Println(i) //outputs 15
+
 }
