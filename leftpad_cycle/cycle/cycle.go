b94f8f039726072c8e9e7445fb604e7f043fedb0
imrachanagupta <rachanagupta04@gmail.com>
2021-11-10 11:54:31 +0530

    cross reference not allowed

---
+++ leftpad_cycle/cycle/cycle.go
@@ -0,0 +1,8 @@
+package cycle
+
+var DEFAULT_CHAR = ' '
+
+func DoublePad(s string, i int) string {
+	return "Hello"
+	//return leftpad.Format(s+s, i) -- This will create cross reference of the packages that is not allowed in GO.
+}
