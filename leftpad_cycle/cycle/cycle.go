42804ad6f250ea3d37d4a80414995df500e40065
imrachanagupta <rachanagupta04@gmail.com>
2021-11-10 12:01:09 +0530

    An example Cross reference of packages. Package cycle is dependent on package leftpad and vice versa. This is not allowed in Go. It will throw error as "import cycle not allowed"

--- leftpad_cycle/cycle/cycle.go
+++ leftpad_cycle/cycle/cycle.go
@@ -1,8 +1,9 @@
 package cycle
 
+import leftpad "github.com/imrachanagupta/GO/leftpad_cycle"
+
 var DEFAULT_CHAR = ' '
 
 func DoublePad(s string, i int) string {
-	return "Hello"
-	//return leftpad.Format(s+s, i) -- This will create cross reference of the packages that is not allowed in GO.
+	return leftpad.Format(s+s, i) // This will create cross reference of the packages that is not allowed in GO.
 }
